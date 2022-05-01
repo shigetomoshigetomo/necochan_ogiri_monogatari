e179294069d68693e9774300c009add4f184cfaf
shigetomo <runorunonyanmyu@gmail.com>
2022-05-01 04:37:18 +0000

    [add]dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Admin.create!(name: "",
-#               email: "",
-#               password: ""
-#               )
+Admin.create!(name: ENV['ADMIN_NAME'],
+              email: ENV['ADMIN_EMAIL'],
+              password: ENV['ADMIN_PASSWORD']
+              )

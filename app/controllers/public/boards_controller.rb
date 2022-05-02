8bf133a910ba249cd8760538d32e688bb8c7cc0c
shigetomo <runorunonyanmyu@gmail.com>
2022-05-02 03:10:59 +0000

    [add]board new

--- app/controllers/public/boards_controller.rb
+++ app/controllers/public/boards_controller.rb
@@ -1,4 +1,6 @@
 class Public::BoardsController < ApplicationController
+  before_action :authenticate_member!, only: [:new, :create]
+
   def index
   end
 
@@ -6,5 +8,22 @@ class Public::BoardsController < ApplicationController
   end
 
   def new
+    @board = Board.new
   end
+
+  def create
+    @board = Board.new(board_params)
+    @board.member_id = current_member.id
+    if @board.save
+      redirect_to board_path(@board), notice: "お題を投稿しました"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def board_params
+      params.require(:board).permit(:title, :member_id)
+    end
 end

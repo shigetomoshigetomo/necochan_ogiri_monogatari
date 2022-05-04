242de117081c04ecf70bdd24cd74b0f782138151
shigetomo <runorunonyanmyu@gmail.com>
2022-05-04 03:20:34 +0000

    [add]admin member

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -14,6 +14,18 @@ class Admin::MembersController < ApplicationController
   end
 
   def update
-
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      flash[:notice] = "#{@member.name}さんの会員ステータスを更新しました"
+      redirect_to admin_member_path(@member)
+    else
+      render "edit"
+    end
   end
+
+  private
+
+    def member_params
+      params.require(:member).permit(:is_deleted)
+    end
 end

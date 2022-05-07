9be80b9f4740556add327e6fde4e2b8b257430ba
shigetomo <runorunonyanmyu@gmail.com>
2022-05-07 14:53:17 +0000

    [update]level

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ Level.create!(level: 1,
               )
 
 level = 1
-for i in 1..230
+for i in 1..199
   new_level = level + i
   before_level = Level.find(new_level - 1)  #一つ前のレベルを探す
   new_threshold = before_level.threshold * 1.1  #一つ前のレベルの闘値に1.1掛ける
@@ -23,3 +23,5 @@ for i in 1..230
                 threshold: new_threshold
                 )
 end
+
+Genre.create!([{name: "武器屋"}, {name: "道具屋"}, {name: "食堂"}])

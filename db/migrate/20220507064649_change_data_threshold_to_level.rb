604ed9dd051684853a015ce8de0d50ed354933e3
shigetomo <runorunonyanmyu@gmail.com>
2022-05-07 07:31:29 +0000

    save

--- db/migrate/20220507064649_change_data_threshold_to_level.rb
+++ db/migrate/20220507064649_change_data_threshold_to_level.rb
@@ -1,5 +1,5 @@
 class ChangeDataThresholdToLevel < ActiveRecord::Migration[6.1]
   def change
-    change_column :levels, :threshold, :float
+    change_column :levels, :threshold, :integer
   end
 end

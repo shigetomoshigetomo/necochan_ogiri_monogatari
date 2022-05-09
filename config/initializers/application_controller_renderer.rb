85c2cd96f5fa64762418191544962b1a5e56acd4
shigetomo <runorunonyanmyu@gmail.com>
2022-05-09 12:32:26 +0000

    save

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,9 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-  ActsAsTaggableOn.force_binary_collation = true
-end
+# ActiveSupport::Reloader.to_prepare do
+# #   ApplicationController.renderer.defaults.merge!(
+# #     http_host: 'example.org',
+# #     https: false
+# #   )
+#   ActsAsTaggableOn.force_binary_collation = true
+# end

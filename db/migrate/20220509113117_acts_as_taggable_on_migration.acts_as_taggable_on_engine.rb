a4f0c1e605cb62a5320ba416beb44e00815d9431
shigetomo <runorunonyanmyu@gmail.com>
2022-05-09 11:45:35 +0000

    save

--- db/migrate/20220509113117_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
+++ db/migrate/20220509113117_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
@@ -23,12 +23,12 @@ class ActsAsTaggableOnMigration < ActiveRecord::Migration[6.0]
       t.datetime :created_at
     end
 
-    # add_index ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type context],
-    #           name: 'taggings_taggable_context_idx'
+    add_index ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type context],
+              name: 'taggings_taggable_context_idx'
   end
 
   def self.down
-    # drop_table ActsAsTaggableOn.taggings_table
-    # drop_table ActsAsTaggableOn.tags_table
+    drop_table ActsAsTaggableOn.taggings_table
+    drop_table ActsAsTaggableOn.tags_table
   end
 end

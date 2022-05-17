b8063456f7890d9181c4d27fa08f83be417570b0
shigetomo <runorunonyanmyu@gmail.com>
2022-05-17 02:51:26 +0000

    [add]admin report index status search

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -2,7 +2,14 @@ class Admin::ReportsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @reports = Report.all.page(params[:page]).per(10)
+    if params[:status] == "all"
+      @reports = Report.all.page(params[:page]).per(10)
+    elsif params[:status]
+      report_statuses = Report.where(status: params[:status])
+      @reports = report_statuses.page(params[:page]).per(10)
+    else
+      @reports = Report.all.page(params[:page]).per(10)
+    end
   end
 
   def show

e511cc96fa593172820e50260db7c9ba0896117a
shigetomo <runorunonyanmyu@gmail.com>
2022-05-17 15:42:24 +0000

    [fix]search member in guest

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,7 +3,7 @@ class Public::SearchesController < ApplicationController
 
   def search
     @word = params[:word]
-    @members = Member.where("name LIKE?","%#{@word}%").page(params[:page]).per(10)
+    @members = Member.not_guest.where("name LIKE?","%#{@word}%").page(params[:page]).per(10)
     @boards = Board.where("title LIKE?","%#{@word}%").page(params[:page]).per(6)
     @posts = Post.where("content LIKE?","%#{@word}%").page(params[:page]).per(6)
   end

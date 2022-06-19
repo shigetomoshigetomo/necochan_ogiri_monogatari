2416e803f1477c5a5f747201a8f706ada91fc21a
shigetomo <runorunonyanmyu@gmail.com>
2022-06-19 06:00:00 +0000

    [fix]search empty send searchcontroller admin

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -3,8 +3,13 @@ class Admin::SearchesController < ApplicationController
 
   def search
     @word = params[:word]
-    @members = Member.not_guest.where("name LIKE?", "%#{@word}%").order(created_at: "ASC")
-    @boards = Board.where("title LIKE?", "%#{@word}%")
-    @posts = Post.where("content LIKE?", "%#{@word}%")
+    if @word.blank?
+      redirect_to request.referer
+      flash[:notice] = "キーワードを入力して下さい。"
+    else
+      @members = Member.not_guest.where("name LIKE?", "%#{@word}%").order(created_at: "ASC")
+      @boards = Board.where("title LIKE?", "%#{@word}%")
+      @posts = Post.where("content LIKE?", "%#{@word}%")
+    end
   end
 end

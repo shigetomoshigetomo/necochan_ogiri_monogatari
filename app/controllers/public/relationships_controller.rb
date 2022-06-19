ef42d3f8d56955c38455944a44085503623ef45f
shigetomo <runorunonyanmyu@gmail.com>
2022-06-19 07:41:44 +0000

    [destroy]relationshipcontroller unusing action [fix]member friends index 繰り返しが二重になっていた所

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -12,14 +12,4 @@ class Public::RelationshipsController < ApplicationController
     @member = Member.find(params[:member_id])
     current_member.unfollow(@member.id)
   end
-
-  def followings
-    member = Member.find(params[:member_id])
-    @members = member.followings
-  end
-
-  def followers
-    member = Member.find(params[:member_id])
-    @members = member.followers
-  end
 end
